eceacbd4393d107806a234e394f6f61f995a5f08
Dundaki <vigjoaopaulo.com>
2021-08-21 08:48:59 -0300

    REQUEST_METHOD FUNCIONANDO

--- APIListarCidades.php
+++ APIListarCidades.php
@@ -1,55 +1,56 @@
 <?php
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 
+    header('Content-type: application/json');
 
-header('Content-type: application/json');
+    require_once('dbConnect.php');
 
-require_once('dbConnect.php');
+    // Definir UTF8 para a conexão
 
-// Definir UTF8 para a conexão
+    mysqli_set_charset($conn, $charset);
 
-mysqli_set_charset($conn, $charset);
+    $response = array();
 
-$response = array();
+    // Prepara a consulta SQL
 
-// Prepara a consulta SQL
-
-$stmt = mysqli_prepare($conn, "SELECT id, nome, estadoID FROM cidade");
+    $stmt = mysqli_prepare($conn, "SELECT id, nome, estadoID FROM cidade");
 
 
-// Executa a consulta
+    // Executa a consulta
 
-mysqli_stmt_execute($stmt);
+    mysqli_stmt_execute($stmt);
 
-// Salva o resultado da consulta
+    // Salva o resultado da consulta
 
-mysqli_stmt_store_result($stmt);
+    mysqli_stmt_store_result($stmt);
 
-// Gera os dados a serem apresentados
-// em variáveis conforme o select.
+    // Gera os dados a serem apresentados
+    // em variáveis conforme o select.
 
-mysqli_stmt_bind_result($stmt, $id, $nome, $estadoID);
+    mysqli_stmt_bind_result($stmt, $id, $nome, $estadoID);
 
-// apresenta os dados da consulta
-// var_dump($stmt);
+    // apresenta os dados da consulta
+    // var_dump($stmt);
 
-if (mysqli_stmt_num_rows($stmt) > 0) {
+    if (mysqli_stmt_num_rows($stmt) > 0) {
 
-    while (mysqli_stmt_fetch($stmt)) {
+        while (mysqli_stmt_fetch($stmt)) {
 
-        array_push($response, array(
-            "id" => $id,
-            "nome" => $nome,
-            "estadoID" => $estadoID));
+            array_push($response, array(
+                "id" => $id,
+                "nome" => $nome,
+                "estadoID" => $estadoID));
+        }
+        
+        echo json_encode($response);
+        
+    } else {
+        
+        echo json_encode($response);
+        
     }
-    
-    echo json_encode($response);
-    
-} else {
-    
-     echo json_encode($response);
-    
 }
 ?>
